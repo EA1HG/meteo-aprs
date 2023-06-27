5fb510a9cf25cd81a05ec2073250f7b141a0414a
EA1HG <ismazg@gmail.com>
2023-06-27 13:40:35 +0200

    Update installwx.sh

--- installwx.sh
+++ installwx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash -c "$(curl -fsSL https://github.com/ea1hg/meteo-aprs/-/raw/main/install.sh)"
+bash -c "$(curl -fsSL https://github.com/EA1HG/meteo-aprs/-/raw/main/install.sh)"
  menu-py-wx
